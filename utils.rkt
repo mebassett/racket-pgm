7c8b744b6b495db57ee4669b5125123457849a42
Matthew Eric Bassett <mebassett@gegn.net>
2017-01-02 23:26:40 -0700

    fixing type signature on set-filter so it can handle predicates.
    	modified:   utils.rkt

--- utils.rkt
+++ utils.rkt
@@ -2,6 +2,9 @@
 
 (provide set-filter)
 
-(: set-filter (All (T) (-> (-> T Boolean) (Setof T) (Setof T))))
+(: set-filter (All (T R)
+                   (case->
+                    (-> (-> T Boolean) (Setof T) (Setof T))
+                    (-> (-> T Boolean : R) (Setof T) (Setof R)))))
 (define (set-filter valid? set)
   (list->set (filter valid? (set->list set))))
