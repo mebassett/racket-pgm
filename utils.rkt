1416a537ae4e2333f97b9b1e196395fcfe985d2c
Matthew Eric Bassett <mebassett@gegn.net>
2017-01-07 23:53:22 -0700

    adding Float versions of the mean function from math/statistics
    	modified:   utils.rkt

--- utils.rkt
+++ utils.rkt
@@ -1,6 +1,6 @@
 #lang typed/racket
 
-(require math/matrix)
+(require math/matrix math/flonum)
 (provide set-filter unpack order-of-elems)
 
 (: set-filter (All (T R)
@@ -26,5 +26,34 @@
            (helper (cdr small-list) (cdr big-list) (add1 count) (append ret (list count)))]
           [else (helper small-list (cdr big-list) (add1 count) ret)]))
   (helper small-list big-list 0 '()))
-    
-                               
+
+; the following are from math/statistics, but the types are changed to Flonum
+
+(: check-lengths! (All (A B) (Symbol String A B Index Index -> Void)))
+(define (check-lengths! name what xs ys m n)
+  (unless (= m n) (error name "~a must be the same length; given ~e (length ~a) and ~e (length ~a)"
+                         what xs m ys n)))
+
+(: sequences->weighted-samples
+   (All (A) (Symbol (Sequenceof A) (Sequenceof Float)
+                    -> (Values (Listof A) (Listof Float)))))
+(define (sequences->weighted-samples name x-seq w-seq)
+  (define xs (sequence->list x-seq))
+  (define ws
+    (for/list: : (Listof Float) ([w w-seq])
+      (cond [(w . >= . 0)  w]
+            [else  (raise-argument-error name "(Sequenceof Nonnegative-Real)" 1 x-seq w-seq)])))
+  (check-lengths! name "values and weights" xs ws (length xs) (length ws))
+(values xs ws))
+
+(: flmean (case-> ((Sequenceof Float) -> Float)
+                ((Sequenceof Float) (Option (Sequenceof Float)) -> Float)))
+(define (flmean xs [ws #f])
+  (cond [ws  (let-values ([(xs ws)  (sequences->weighted-samples 'mean xs ws)])
+               (define n (flsum ws))
+               (cond [(zero? n)  +nan.0]
+                     [else  (/ (flsum (map * xs ws)) n)]))]
+        [else  (let ([xs  (sequence->list xs)])
+                 (define n (length xs))
+                 (cond [(zero? n)  +nan.0]
+                       [else (/ (flsum xs) n)]))]))                               
